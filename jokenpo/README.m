55a51f33ccf9aa51823993324b1d75714462c0d5
Julia Gusson <juliagdeamo@gmail.com>
2025-04-01 19:40:30 -0300

    Criando README do Jokenpô

---
+++ jokenpo/README.m
@@ -0,0 +1,17 @@
+# Jokenpô com Validação de Idade  
+
+Este projeto é um jogo de Pedra, Papel e Tesoura desenvolvido durante a Imersão Dev da Alura.  
+
+## 🚀 Como funciona?  
+- O usuário informa sua idade antes de jogar.  
+- Se for menor de 12 anos, não pode acessar o jogo.  
+- O jogo acontece contra a "Ju", que faz uma escolha aleatória.  
+- No final, é mostrado quem venceu e qual foi a jogada do computador.  
+
+## 🛠️ Tecnologias usadas  
+- HTML  
+- CSS  
+- JavaScript  
+
+## 🔗 Link para testar  
+[Jogue no CodePen](https://codepen.io/Julia-Gusson/pen/PwoXWzr)  
